9c8dbe9a4d169473276e467f5d4f6c514f91f852
gukaifeng <892859816@qq.com>
2023-12-27 14:23:35 +0800

    remove or disable options that are not currently supported on the register page

--- src/views/login/userRegister.vue
+++ src/views/login/userRegister.vue
@@ -2,7 +2,7 @@
 	<common-page title="注册新账号">
 		<el-steps :active="stepActive" simple finish-status="success">
 		    <el-step title="基础信息" />
-			<el-step title="详细信息" />
+			<!-- <el-step title="详细信息" /> -->
 		    <el-step title="完成注册" />
 		</el-steps>
 		<el-form v-if="stepActive==0" ref="stepForm_0" :model="form" :rules="rules" :label-width="120">
@@ -22,7 +22,7 @@
 				<el-checkbox v-model="form.agree" label="">已阅读并同意</el-checkbox><span class="link" @click="showAgree=true">《平台服务协议》</span>
 			</el-form-item>
 		</el-form>
-		<el-form v-if="stepActive==1" ref="stepForm_1" :model="form" :rules="rules" :label-width="120">
+		<!-- <el-form v-if="stepActive==1" ref="stepForm_1" :model="form" :rules="rules" :label-width="120">
 			<el-form-item label="真实姓名" prop="userName">
 				<el-input v-model="form.userName" placeholder="请输入真实姓名"></el-input>
 			</el-form-item>
@@ -42,8 +42,8 @@
 					<el-checkbox label="3">Javescript API</el-checkbox>
 				</el-checkbox-group>
 			</el-form-item>
-		</el-form>
-		<div v-if="stepActive==2">
+		</el-form> -->
+		<div v-if="stepActive==1">
 			<el-result icon="success" title="注册成功" sub-title="可以使用登录账号以及手机号登录系统">
 				<template #extra>
 					<el-button type="primary" @click="goLogin">前去登录</el-button>
@@ -51,9 +51,9 @@
 			</el-result>
 		</div>
 		<el-form style="text-align: center;">
-			<el-button v-if="stepActive>0 && stepActive<2" @click="pre">上一步</el-button>
-			<el-button v-if="stepActive<1" type="primary" @click="next">下一步</el-button>
-			<el-button v-if="stepActive==1" type="primary" @click="save">提交</el-button>
+			<!-- <el-button v-if="stepActive>0 && stepActive<2" @click="pre">上一步</el-button>
+			<el-button v-if="stepActive<1" type="primary" @click="next">下一步</el-button> -->
+			<el-button v-if="stepActive==0" type="primary" @click="save">提交</el-button>
 		</el-form>
 		<el-dialog v-model="showAgree" title="平台服务协议" :width="800" destroy-on-close>
 			平台服务协议
